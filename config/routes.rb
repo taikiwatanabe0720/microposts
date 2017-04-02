95393a3e938484b92438e1abfacf4bed4b169286
taiki watanabe <taikiwata0720@gmail.com>
2017-04-02 13:15:49 +0000

    add followers and followings page

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,13 @@ Rails.application.routes.draw do
   get    'login' , to: 'sessions#new'
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
+
   resources :microposts
-  resources :users
+  resources :users do
+    member do
+      get :followings, :followers 
+    end
+  end
+  
   resources :relationships, only: [:create, :destroy]
 end
